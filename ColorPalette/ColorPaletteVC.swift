62d1bdc78dbcf82df7860dc37bc44ba339491b5c
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-19 16:37:58 +0300

    UITableView Delegate and DataSoruce is implemented. Basic navigation is handled

--- ColorPalette/ColorPaletteVC.swift
+++ ColorPalette/ColorPaletteVC.swift
@@ -17,3 +17,18 @@ class ColorPaletteVC: UIViewController {
 
 }
 
+extension ColorPaletteVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ToColorDetailVC", sender: nil)
+    }
+    
+}
+
