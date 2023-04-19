d4025350756fdb4cd5d7717a21ecea301d4e8e3c
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-19 16:32:53 +0300

    ViewControllers are setup and tableView is added

---
+++ ColorPalette/ColorDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  ColorDetailVC.swift
+//  ColorPalette
+//
+//  Created by Ömer Faruk Okumuş on 19.04.2023.
+//
+
+import UIKit
+
+class ColorDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
