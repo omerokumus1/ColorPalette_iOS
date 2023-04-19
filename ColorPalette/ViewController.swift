7c38f2d56ff9272e77e2764bd0ab129f2c66b370
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-19 16:23:10 +0300

    Initial Commit

---
+++ ColorPalette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorPalette
+//
+//  Created by Ömer Faruk Okumuş on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
