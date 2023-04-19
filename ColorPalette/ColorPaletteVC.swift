42ea3c7a48dbe7a2b5c5a1ff3ad3e58d7c2cb669
Ömer Okumuş <omerokumus3@gmail.com>
2023-04-19 17:09:31 +0300

    ColorPalette is populated. Navigation is handled

--- ColorPalette/ColorPaletteVC.swift
+++ ColorPalette/ColorPaletteVC.swift
@@ -8,26 +8,51 @@
 import UIKit
 
 class ColorPaletteVC: UIViewController {
-
+    
+    var colorPalette: [UIColor] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        populateColorPalette()
     }
-
-
+    
+    private func populateColorPalette() {
+        for _ in 0..<50 {
+            colorPalette.append(getRandomColor())
+        }
+    }
+    
+    private func getRandomColor() -> UIColor {
+        return UIColor(red: CGFloat.random(in: 0...1),
+                       green: CGFloat.random(in: 0...1),
+                       blue: CGFloat.random(in: 0...1),
+                       alpha: 1)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToColorDetailVC" {
+            let vc = segue.destination as! ColorDetailVC
+            guard let color = sender as? UIColor else { return }
+            vc.color = color
+        }
+    }
 }
 
 extension ColorPaletteVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return colorPalette.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell") else {
+            return UITableViewCell()
+        }
+        cell.backgroundColor = colorPalette[indexPath.row]
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "ToColorDetailVC", sender: nil)
+        performSegue(withIdentifier: "ToColorDetailVC", sender: colorPalette[indexPath.row])
     }
     
 }
